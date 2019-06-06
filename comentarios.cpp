4986ee63d77665b3dac31844d4dcdbca2979ca68
Floricela <floryperez123@outlook.es>
2019-06-06 01:33:11 -0500

    comentario

---
+++ comentarios.cpp
@@ -0,0 +1,32 @@
+// y aquí está el cambio == ya viste???? ok
+// y así si neceitas que revisemos algo es muy sencillo 
+//
+// para subir cambios 
+// es con 
+// git push origin master
+// y para descargar 
+//es con
+// git pull
+
+
+/*
+antes de subir cambios primero tienes que seleccionar qué archivo guardar
+con: 
+git add nombre_del_archivo_con_extension
+ejemplo: git add hola.txt
+o el mismo que estoy modificando
+1. git add venta.c
+
+// luego le pones una etiqueta con::
+git commit -m "indicas los cambios que hiciste así"
+
+ejemplo: 
+2. git commit -m "agregué indicaciones para subir y bajar archivos de github"
+
+3. git push origin master
+
+-- vale?? 
+// guarda estos comentarios en un archivo y lo subes haber todo lo que puse ahora voy
+*/
+// ya veo tu programa y lo modifico sin problema  
+
