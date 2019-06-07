949d04e4815689d3087ccb94cec4041eb39c6184
Floricela <floryperez123@outlook.es>
2019-06-06 23:37:23 -0500

    cambios

--- venta.c
+++ venta.c
@@ -1,47 +1,60 @@
 #include <stdio.h>
 #include <string.h>
-#define VENDEDOR 5
+#define NOVENDEDOR 5
+#define MES 3
 #define LONGCAD 30
+#define LONGPO 5
+
+typedef  struct datosvendedor
+{
+char nombre [LONGCAD];
+char mes [LONGCAD];
+
+}vendedor;
+
+
+
+//definicion de funciones
 int menu();
 int submenu();
-void leer_Datos( );
+void leer_ventas( vendedor[] );
+void leervendedor(vendedor *);
 
-void consultas();
+void reportes (vendedor[]);
 
-void individual(float);
+void reporteGral(vendedor[]);
+void reporteindividual(vendedor []);
 
-void total_mes(char, char );
+void imprimirvendedor(vendedor);
+void calculatotaldeventa();
+void vendedor_maximo();
+void vendedor_minimo();
+void mes_maximo ();
+void mes_minimo();
+void venta_anual(vendedor[]);
+void  reporteestadistica(vendedor);
 
-void total_vendedor(char,char);
 
-void vendedor_maximo(char ,char);
 
-void vendedor_minimo(char, char);
-
-void mes_maximo(float);
-
-void mes_minimo (float);
-void venta_anual(float);
 
 
 int main()
 {
 
-	
-	
+vendedor ven [NOVENDEDOR] ;
+
   int opc;
   do{
         opc=menu();
         switch(opc)
         {
             case 1:
-                leer_Datos();
+                leer_Datos(ven);
                 break;
             case 2:
-              consultas ()  ;
-                break;
-            
-            default:
+              reportes (ven)  ;
+           default:
+            	
 	puts("Opcion no valida \n");
         }
     }while (opc!=3);
@@ -53,10 +66,10 @@ int menu()
 {
   int opcion;
 
-    
+
 puts("----Menu Principal \n");
-puts ("1.Leer datos\n ");
-puts ("2.consultas \n ");
+puts ("1.Leer ventas\n ");
+puts ("2.generar reportes \n ");
 puts("3.- Salir\n\n");
   puts("Seleccione una opcion: ");
   scanf("%d", &opcion);
@@ -68,41 +81,142 @@ puts("3.- Salir\n\n");
 int submenu()
 {
   int opcion;
-	puts ("1.consultas individuales\n ");
-	puts ("2.consultar total del mes \n ");
-	puts ("3.consultar vendedor maximototal del mes\n" );
-	puts ("4.consultar vendedor minimo\n" );
-	puts ("5.consultar mes maximo\n" );
-	puts ("6.consultar mes  minimo\n" );
-	puts ("7.consultar venta anual \n" );
-	puts("8.- Regresar al menu principal\n\n");
+	puts ("1.reporte general\n ");
+	puts ("2.reporte individual \n ");
+	puts ("3.estadistica\n" );
+    puts("4.- Regresar al menu principal\n\n");
 	puts("Seleccione una opcion: ");
-    
+
     scanf("%d", &opcion);
 
   return opcion;
 }
 
 
-void leerDatos(char VENDEDOR [][LONGCAD])
+
+void leer_ventas(  vendedor ven  [NOVENDEDOR])
 {
-	int i,j;
-	for(i=0;i<VENDEDOR;i++)
+	int i;
+	for(i=0;i<NOVENDEDOR;i++)
 	{
 		printf("Datos del vendedor  %d \n",i+1);
 		printf("Nombre: ");
-		setbuf(stdin,NULL);
-		
+     	leervendedor(&ven[i]);
+
 		}
-		
+
+}
+
+
+void leervendedor(vendedor *ven)
+{
+	int j;
+	printf("Nombre: ");
+	scanf("%[^\n]",ven->nombre);
+	printf("ingrese mes: ");
+	scanf("%[^\n]",ven->mes);
+
 }
 
 
 
-void consultas()
+
+
+
+
+void reportes (vendedor ven[NOVENDEDOR])
 {
+
 	int opc;
 	do{
 		opc=submenu();
-	}while(opc!=8);
+		switch(opc)
+        {
+            case 1:
+                reporteGral(ven);
+                break;
+            case 2:
+				reporteindividual(ven);
+                break;
+            case 3:
+                /*reporteestadistica(ven);*/
+                break;
+
+
+            default:
+				puts("Opcion no valida");
+        }
+	}while(opc!=4);
 }
+
+
+
+void reporteGral(vendedor ven[NOVENDEDOR])
+
+{
+int i;
+puts("Nombre\t\t\tmes\t\t\t");
+	for(i=0;i<NOVENDEDOR;i++)
+	{
+		imprimirvendedor(ven[i]);
+		puts("");
+	}
+
+
+
+}
+void imprimirvendedor(vendedor ven)
+{
+	int j;
+	printf("%s\t\t\t%s\t\t",ven.nombre,ven.mes);
+}
+
+
+void reportemes(vendedor ven[NOVENDEDOR])
+{
+	char dato[LONGPO];
+	int i, encontrado=0;
+	printf("Proporcione el MES del que desea el reporte: ");
+	
+	scanf("%[^\n]",dato);
+	
+	puts("Nombre\t\t\tMES\t\t");
+	for(i=0;i<MES;i++)
+	{
+		if (strcmp(dato,ven[i].mes)==0){
+			encontrado=1;
+			imprimirvendedor(ven[i]);
+			puts("");
+		}
+	}
+	if(encontrado==0)
+	puts("No se encontró el mes");
+}
+
+
+
+
+
+void venta_anual(vendedor  ven[NOVENDEDOR])
+{}
+
+
+
+void calculatotaldeventa()
+{}
+
+
+/*
+void reporteindividual(vendedor ven [NOVENDEDOR])
+{
+
+int i;
+	float totalindividual;
+
+	for(i=0;i<NOVENDEDOR;i++)
+	{
+		totalindividual=(ven[i]);
+
+	}
+}*/
+
